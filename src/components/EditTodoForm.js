866800e258db4cffe90a783dac5fa1848fba0461
Kyle <kyle.w.stambaugh1@gmail.com>
2023-10-10 12:04:04 -0400

    finished todo list tutorial

---
+++ src/components/EditTodoForm.js
@@ -0,0 +1,26 @@
+import React, {useState} from 'react'
+
+export const EditTodoForm = ({editTodo, task}) => {
+
+    const [value, setValue] = useState(task.task)
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        editTodo(value, task.id)
+
+        setValue("")
+    }
+
+  return (
+    <form className='TodoForm' onSubmit={handleSubmit}>
+        <input type='text' 
+            value={value}
+            className='todo-input' 
+            placeholder='What is the task today?'
+            onChange={(e)=> setValue(e.target.value)}/>
+        <button type='submit' className='todo-btn'>
+            Update Task
+        </button>
+        </form>
+  )
+}
